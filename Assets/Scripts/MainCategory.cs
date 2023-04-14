2e73de210ac7899317dd3f6d27e7ae8ccbd06bc9
MateenAlamCross <127185542+MateenAlamCross@users.noreply.github.com>
2023-04-14 17:19:59 +0500

    Improve Code
    Adding More methods for Other Categories

--- Assets/Scripts/MainCategory.cs
+++ Assets/Scripts/MainCategory.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -8,7 +9,8 @@ using UnityEngine.UI;
 public class MainCategory : MonoBehaviour
 {
     // public Image Image;
-    
+    GameObject hotitems;
+    private GameObject clothing;
     public void SpawnCategory()
     {
         GameObject buttonTemplate = transform.GetChild(0).gameObject;
@@ -18,16 +20,50 @@ public class MainCategory : MonoBehaviour
         {
             objectInstance = Instantiate(buttonTemplate, transform);
             Texture2D myTexture = Resources.Load(GameManager.instance.rootModel.data[i].mainCategoryImage) as Texture2D;
-            // Debug.Log(g.transform);
-            // Debug.Log(GameManager.instance.rootModel.data[i].mainCategoryImage);
+            
             objectInstance.transform.GetComponent<Image>().sprite = Sprite.Create(myTexture,
                 new Rect(0, 0, myTexture.width, myTexture.height), new Vector2(0.5f, 0.5f));
             objectInstance.transform.gameObject.name = GameManager.instance.rootModel.data[i].mainCategoryName;
         }
 
         Destroy(buttonTemplate);
+        hotitems = GameObject.Find("hotItems");
+       
+        hotitems.GetComponent<Button>().onClick.AddListener(OnPressHotItems);
+        clothing = GameObject.Find("coat");
+        clothing.GetComponent<Button>().onClick.AddListener(DestroyAllSpawned);
     }
 
-   
+
+
+    public void OnPressHotItems()
+    { 
+        SpawnCash.instance.DestroyAllSpawned();
+
+        SpawnCash.instance.spawned = true;
+        SpawnCash.instance.SpawnPayment("hotItems");
+    }
+
+    public void DestroyAllSpawned()
+    {
+        
+        SpawnCash.instance.DestroyAllSpawned();
+        SpawnCash.instance.spawned = true;
+
+        SpawnCash.instance.SpawnCoat();
+
+    }
     
 }
+
+
+
+
+
+// foreach(GameObject gameObj in GameObject.FindObjectsOfType<GameObject>())
+// {
+//     if(gameObj.name == "payment")
+//     {
+//         Destroy(gameObj);
+//     }
+// }
--- Assets/Scripts/SpawnCash.cs
+++ Assets/Scripts/SpawnCash.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,40 +6,196 @@ using UnityEngine.UI;
 
 public class SpawnCash : MonoBehaviour
 {
-    public void SpawnPayment()
-    {
-        GameObject buttonTemplate = transform.GetChild(0).gameObject;
-        GameObject objectInstance;
-            
-        for (int i = 0; i < GameManager.instance.rootModel.data.Count; i++)
-        {
-            if (GameManager.instance.rootModel.data[i].mainCategoryName == "hotItems")
-            {
-                int count = GameManager.instance.rootModel.data[i].subCategory.Count;
-                Debug.Log(count);
-                for (int j = 0; j < count; j++)
-                {
-                    if(GameManager.instance.rootModel.data[i].subCategory[j].subCategoryName == "all")
-                    {
-                        Debug.Log(GameManager.instance.rootModel.data[i].subCategory[j].subCategoryName );
-                        for (int k = 0; j < GameManager.instance.rootModel.data[i].subCategory[j].items.Count; k++)
-                        {
-                            objectInstance = Instantiate(buttonTemplate, transform);
-                            Debug.Log(objectInstance.transform.GetChild (0).gameObject);
-                            Debug.Log(GameManager.instance.rootModel.data[i].subCategory[j].items.Count);
-                            Texture2D myTexture = Resources.Load(GameManager.instance.rootModel.data[i].subCategory[j].items[k].paymentType) as Texture2D;
-                            objectInstance.transform.GetChild (0).GetComponent <Image> ().sprite = Sprite.Create(myTexture,
-                                new Rect(0, 0, myTexture.width, myTexture.height), new Vector2(0.5f, 0.5f));;
-                            objectInstance.transform.GetChild (1).GetComponent <Text> ().text = GameManager.instance.rootModel.data[i].subCategory[j].items[k].price.ToString();
-                        }
-                    }
-                    
-                }
-       
-            }
-           
-        }
+    public static SpawnCash instance;
+    public bool spawned ;
 
-        Destroy(buttonTemplate);
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+    }
+
+
+    public void DestroyAllSpawned()
+    {
+        foreach (Transform child in transform)
+        {
+            Destroy(child.gameObject);
+        }
+    }
+
+    public void SpawnPayment(string name)
+    {
+        if (spawned == true)
+        {
+            
+        
+            GameObject buttonTemplate = transform.GetChild(0).gameObject;
+            GameObject objectInstance;
+                
+            for (int i = 0; i < GameManager.instance.rootModel.data.Count; i++)
+            {
+                if (GameManager.instance.rootModel.data[i].mainCategoryName == name )
+                // if (GameManager.instance.rootModel.data[i].mainCategoryName == "hotItems" )
+                {
+                    int count = GameManager.instance.rootModel.data[i].subCategory.Count;
+                    for (int j = 0; j < count; j++)
+                    {
+                        if(GameManager.instance.rootModel.data[i].subCategory[j].subCategoryName == "all")
+                        {
+                            Debug.Log("Category Name = " + GameManager.instance.rootModel.data[i].subCategory[j].subCategoryName );
+                            for (int k = 0; k < GameManager.instance.rootModel.data[i].subCategory[j].items.Count; k++)
+                            {
+
+                                objectInstance = Instantiate(buttonTemplate, transform);
+                                Debug.Log("i : "+ i + ", j : "+ j+", K: "+ k);
+                                
+                                
+                                
+                                // Payment Icon Refrence Passing
+                              Texture2D paymentTypeIcon = Resources.Load(GameManager.instance.rootModel.data[i].subCategory[j]
+                              .items[k].paymentType.ToString()) as Texture2D; 
+                              objectInstance.transform.GetChild(0).GetComponent<RawImage>().texture = paymentTypeIcon;
+                              
+                             
+
+                              //Price In term of Coin & Diamonds
+                                objectInstance.transform.GetChild (1).GetComponent <Text> ().text = GameManager.instance.rootModel.data[i].subCategory[j].items[k].price.ToString();
+                                
+                                // Passing Objects references which User will Buy 
+                                Texture2D objectBuyIcon = Resources.Load(GameManager.instance.rootModel.data[i].subCategory[j]
+                                    .items[k].icon.ToString()) as Texture2D; 
+                                objectInstance.transform.GetChild(2).GetComponent<RawImage>().texture = objectBuyIcon;
+                                
+                                // Nameing the spawning Object In term of Coin  OR Diamond
+                                objectInstance.transform.gameObject.name = objectBuyIcon.name;
+
+                                
+                                objectInstance.transform.GetChild(0).GetComponent<RawImage>().enabled = true;
+                                objectInstance.transform.GetChild(1).GetComponent<Text>().enabled = true;
+                                objectInstance.transform.GetChild(2).GetComponent<RawImage>().enabled = true;
+                            }
+                        }
+                        
+                    }
+                }
+               
+            }
+            Destroy(buttonTemplate);
+        }
+        spawned = false;
+    }
+
+    public void SpawnShirt()
+    {
+        
+         if (spawned == true)
+        {
+            GameObject buttonTemplate = transform.GetChild(0).gameObject;
+            GameObject objectInstance;
+                
+            for (int i = 0; i < GameManager.instance.rootModel.data.Count; i++)
+            {
+                if (GameManager.instance.rootModel.data[i].mainCategoryName == "shirt" )
+                {
+                    int count = GameManager.instance.rootModel.data[i].subCategory.Count;
+                    for (int j = 0; j < count; j++)
+                    {
+                        Debug.Log("Category Name = " + GameManager.instance.rootModel.data[i].subCategory.Count );
+                         
+
+                                objectInstance = Instantiate(buttonTemplate, transform);
+
+                                // Payment Icon Refrence Passing
+                              Texture2D paymentTypeIcon = Resources.Load(GameManager.instance.rootModel.data[i].subCategory[j].paymentType.ToString()) as Texture2D; 
+                              objectInstance.transform.GetChild(0).GetComponent<RawImage>().texture = paymentTypeIcon;
+
+                              
+                                //Price In term of Coin & Diamonds
+                                objectInstance.transform.GetChild (1).GetComponent <Text> ().text = GameManager.instance.rootModel.data[i].subCategory[j].price.ToString();
+                                // Passing Objects references which User will Buy 
+                                Texture2D objectBuyIcon = Resources.Load(GameManager.instance.rootModel.data[i].subCategory[j].icon.ToString()) as Texture2D; 
+                                objectInstance.transform.GetChild(2).GetComponent<RawImage>().texture = objectBuyIcon;
+                                
+                                
+                                // Nameing the spawning Object In term of Coin  OR Diamond
+                                objectInstance.transform.gameObject.name = objectBuyIcon.name;
+
+                                objectInstance.transform.GetChild(0).GetComponent<RawImage>().enabled = true;
+                                objectInstance.transform.GetChild(1).GetComponent<Text>().enabled = true;
+                                objectInstance.transform.GetChild(2).GetComponent<RawImage>().enabled = true;
+                    }
+                }
+               
+            }
+            Destroy(buttonTemplate);
+        }
+        spawned = false;
+    }
+
+    public void SpawnDress()
+    {
+        
+    }
+
+    public void SpawnCoat()
+    {
+        if (spawned == true)
+        {
+            
+        
+            GameObject buttonTemplate = transform.GetChild(0).gameObject;
+            GameObject objectInstance;
+                
+            for (int i = 0; i < GameManager.instance.rootModel.data.Count; i++)
+            {
+                if (GameManager.instance.rootModel.data[i].mainCategoryName == "coat" )
+                {
+                    int count = GameManager.instance.rootModel.data[i].subCategory.Count;
+                    for (int j = 0; j < count; j++)
+                    {
+                        Debug.Log("Category Name = " + GameManager.instance.rootModel.data[i].subCategory.Count );
+                         
+
+                                objectInstance = Instantiate(buttonTemplate, transform);
+
+                                // Payment Icon Refrence Passing
+                              Texture2D paymentTypeIcon = Resources.Load(GameManager.instance.rootModel.data[i].subCategory[j].paymentType.ToString()) as Texture2D; 
+                              objectInstance.transform.GetChild(0).GetComponent<RawImage>().texture = paymentTypeIcon;
+
+                          
+                              
+                                //Price In term of Coin & Diamonds
+                                objectInstance.transform.GetChild (1).GetComponent <Text> ().text = GameManager.instance.rootModel.data[i].subCategory[j].price.ToString();
+                                // Passing Objects references which User will Buy 
+                                Texture2D objectBuyIcon = Resources.Load(GameManager.instance.rootModel.data[i].subCategory[j].icon.ToString()) as Texture2D; 
+                                objectInstance.transform.GetChild(2).GetComponent<RawImage>().texture = objectBuyIcon;
+                                
+                                // Nameing the spawning Object In term of Coin  OR Diamond
+                                objectInstance.transform.gameObject.name = objectBuyIcon.name;
+
+                                
+                                objectInstance.transform.GetChild(0).GetComponent<RawImage>().enabled = true;
+                                objectInstance.transform.GetChild(1).GetComponent<Text>().enabled = true;
+                                objectInstance.transform.GetChild(2).GetComponent<RawImage>().enabled = true;
+                    }
+                }
+               
+            }
+            Destroy(buttonTemplate);
+        }
+        spawned = false;
+    }
+
+    public void SpawnGlasses()
+    {
+        
+    }
+
+    public void SpawnJacket()
+    {
+        
     }
 }
